6e8c93d185e68d217eff701864c3d8de3199ce3f
Teddy <teddykucaba@yahoo.com>
2024-04-21 17:20:38 -0500

    Initial transactions

--- PiggyBank/components/POS/CheckOut.js
+++ PiggyBank/components/POS/CheckOut.js
@@ -1,60 +1,92 @@
-// Checkout.js
-// import React from 'react';
-// import { View, Text, StyleSheet } from 'react-native';
-// import { useCart } from './CartContext';
-
-// const Checkout = () => {
-//   const { cart } = useCart();
-
-//   const calculateTotal = () => {
-//     return cart.reduce((acc, curr) => acc + curr.price, 0).toFixed(2);
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.title}>Checkout</Text>                                 
-//       {cart.map((item, index) => (
-//         <Text key={index}>{item.name} - ${item.price}</Text>
-//       ))}
-//       <Text style={styles.total}>Total: ${calculateTotal()}</Text>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     padding: 10,
-//   },
-//   title: {
-//     fontSize: 24,
-//     marginBottom: 20,
-//   },
-//   total: {
-//     marginTop: 20,
-//     fontSize: 18,
-//     fontWeight: 'bold',
-//   },
-// });
-
-// export default Checkout;
-
-
-
-
-
-
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-//import { useCart } from './CartContext';
- import { useCart } from './CartContext';
- const baseURL = process.env.EXPO_PUBLIC_BASE_URL_API;
+import { View, Text, TouchableOpacity, StyleSheet, Alert} from 'react-native';
+import { useCart } from './CartContext';
+import {
+  authorizeAsync,
+  AuthorizeErrorNoNetwork,
+  UsageError,
+  startCheckoutAsync,
+  CheckoutErrorCanceled,
+  CheckoutErrorSdkNotAuthorized,
+  startReaderSettingsAsync,
+  ReaderSettingsErrorSdkNotAuthorized,
+} from 'react-native-square-reader-sdk';
+const baseURL = process.env.EXPO_PUBLIC_BASE_URL_API;
+const AUTH_CODE = "sq0acp-xYL6Fiiu9jxAt0vYiHbwP4zsSMWCveLes4OaVyQmGMo";
 
- const Checkout = () => {
-//const Checkout = ({ useCart }) => {
-  //const { cart } = useCart();
-     const { cart } = useCart();
+const Checkout = () => {
+  const { cart } = useCart();
 
+  const checkoutParams = {
+    amountMoney: {
+      amount: 100,
+      currencyCode: 'USD', // optional, use authorized location's currency code by default
+    }
+  };
+
+  const checkoutSquare = async (authCode, isCard) => {
+    try {
+      // authCode is a mobile authorization code from the Mobile Authorization API
+      const authorizedLocation = await authorizeAsync(authCode);
+      // Authorized and authorizedLocation is available
+    } catch(ex) {
+      switch(ex.code) {
+        case AuthorizeErrorNoNetwork:
+          // Remind connecting to network
+          break;
+        case UsageError:
+          let errorMessage = ex.message;
+          if (__DEV__) {
+            errorMessage += `\n\nDebug Message: ${ex.debugMessage}`;
+            console.log(`${ex.code}:${ex.debugCode}:${ex.debugMessage}`)
+          }
+          Alert.alert('Error', errorMessage);
+          break;
+      }
+    }
+
+    if (isCard) {
+      try {
+        await startReaderSettingsAsync();
+      } catch (ex) {
+        switch(ex.code) {
+          case ReaderSettingsErrorSdkNotAuthorized:
+            // Handle reader settings not authorized
+            break;
+          case UsageError:
+            let errorMessage = ex.message;
+            if (__DEV__) {
+              errorMessage += `\n\nDebug Message: ${ex.debugMessage}`;
+              console.log(`${ex.code}:${ex.debugCode}:${ex.debugMessage}`)
+            }
+            Alert.alert('Error', errorMessage);
+            break;
+        }
+      }
+    }
+
+    try {
+      const checkoutResult = await startCheckoutAsync(checkoutParams);
+      // checkout finished successfully and checkoutResult is available
+    } catch(ex) {
+      switch(ex.code) {
+        case CheckoutErrorCanceled:
+          // Handle canceled transaction here
+          break;
+        case CheckoutErrorSdkNotAuthorized:
+          // Handle sdk not authorized
+          break;
+        default:
+          let errorMessage = ex.message;
+          if (__DEV__) {
+            errorMessage += `\n\nDebug Message: ${ex.debugMessage}`;
+            console.log(`${ex.code}:${ex.debugCode}:${ex.debugMessage}`)
+          }
+          Alert.alert('Error', errorMessage);
+          break;
+      }
+    }
+  };
 
   const calculateTotal = () => {
     return cart.reduce((acc, curr) => acc + curr.price, 0).toFixed(2);
@@ -70,10 +102,10 @@ import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
         <Text style={styles.total}>Total: ${calculateTotal()}</Text>
       </View>
       <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={[styles.button, styles.cashButton]}>
+        <TouchableOpacity style={[styles.button, styles.cashButton]} onPress={() => checkoutSquare(AUTH_CODE, false)} >
           <Text style={styles.buttonText}>Cash</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.button, styles.cardButton]}>
+        <TouchableOpacity style={[styles.button, styles.cardButton]} onPress={() => checkoutSquare(AUTH_CODE, true)}>
           <Text style={styles.buttonText}>Card</Text>
         </TouchableOpacity>
         <TouchableOpacity style={[styles.button, styles.loyaltyButton]}>
